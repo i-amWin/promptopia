326d7b36f4439b46e993536d54f13caff1e5fcf0
Jeet Gangwar <jeetgangwar32@gmail.com>
2023-05-25 01:04:18 +0530

    Tutorial part complete

---
+++ components/Feed.jsx
@@ -0,0 +1,56 @@
+"use client";
+import { useState, useEffect } from "react";
+import PromptCard from "./PromptCard";
+
+const PromptCardList = ({ data, handleTagClick }) => {
+  return (
+    <div className="prompt_layout mt-16">
+      {data.map((post) => {
+        return (
+          <PromptCard
+            key={post._id}
+            post={post}
+            handleTagClick={handleTagClick}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+const Feed = () => {
+  const [searchText, setSearchText] = useState("");
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch("/api/prompt/");
+      const data = await response.json();
+
+      setPosts(data);
+    };
+
+    fetchPosts();
+  }, []);
+
+  const handleSearchChange = (e) => {};
+
+  return (
+    <section className="feed">
+      <form className="flex-center relative w-full">
+        <input
+          type="text"
+          placeholder="Search for a tag or a username"
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+          className="search_input peer"
+        />
+      </form>
+
+      <PromptCardList data={posts} handleTagClick={() => {}} />
+    </section>
+  );
+};
+
+export default Feed;
